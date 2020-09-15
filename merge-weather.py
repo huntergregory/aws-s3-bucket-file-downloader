234c6fbdafdc05e7d3a79674de1d90a00b8bd2be
Hunter Gregory <hlg16@duke.edu>
2020-09-15 05:13:48 -0400

    fixed bug of incrementing weather index no matter what

--- merge-weather.py
+++ merge-weather.py
@@ -17,10 +17,9 @@ def references_weather_type(col_name):
 
 RESULTING_COLUMNS = FINAL_COLUMNS + WEATHER_COLUMNS + [col for col in TIME_SINCE_COLUMNS if not references_weather_type(col)] + [TIME_SINCE_WEATHER_TYPE]
 
-FOLDER = 'final-bss-data'
 def add_nearest_weather(city):
     print('starting bss csv download')
-    bss = pd.read_csv('../{}/{}_bss.csv'.format(FOLDER, city))
+    bss = pd.read_csv('../final-bss-data/{}_bss.csv'.format(city))
     print('finished downloading bss csv')
     bss = bss.sort_values(START_DATETIME)
     weather = pd.read_csv('{}-updated-weather.csv'.format(city))
@@ -32,18 +31,23 @@ def add_nearest_weather(city):
     print('starting loop')
     for bss_index in tqdm(range(0, len(bss))):
         start_datetime = datetime.strptime(bss[START_DATETIME].iloc[bss_index], '%Y-%m-%d %H:%M:%S')
+
+        def get_diff(index):
+            weather_datetime = datetime.strptime(weather.DATE.iloc[index], '%Y-%m-%dT%H:%M:%S')
+            delta = weather_datetime - start_datetime
+            return (delta.days * 24 + delta.seconds / 3600) # hours
+
         differences = []
         for k in range(0, len(weather_indices)):
             weather_index = weather_indices[k]
             if weather_index == len(weather) - 1:
                 continue
-            difference = np.Infinity # in minutes
+
+            difference = get_diff(weather_index) # in minutes
             while weather_index < len(weather):
                 if not weather_na[WEATHER_COLUMNS[k]].iloc[weather_index]:
-                    weather_datetime = datetime.strptime(weather.DATE.iloc[weather_index], '%Y-%m-%dT%H:%M:%S')
-                    delta = weather_datetime - start_datetime
-                    new_difference = delta.days * 86400 + delta.seconds
-                    if abs(new_difference) > abs(difference):
+                    new_difference = get_diff(weather_index)
+                    if abs(new_difference) >= abs(difference):
                         break
                     difference = new_difference
                 weather_index += 1
@@ -51,7 +55,6 @@ def add_nearest_weather(city):
                 weather_index = len(weather) - 1
                 print('[WARN] weather index for {} reached last row at bss_index {}'.format(WEATHER_COLUMNS[k], bss_index))
                 print('start date: {} \t final weather date: {}'.format(start_datetime, datetime.strptime(weather.DATE.iloc[weather_index], '%Y-%m-%dT%H:%M:%S')))
-                print('difference: {}'.format(difference))
             weather_indices[k] = weather_index
             differences.append(difference)
         weather_values = {col_name: weather[col_name].iloc[i] for col_name, i in zip(WEATHER_COLUMNS, weather_indices)}
@@ -69,14 +72,19 @@ def add_nearest_weather(city):
     result = result[RESULTING_COLUMNS]
     result.to_csv('complete_{}_bss.csv'.format(city))
 
-# add_nearest_weather('columbus')
-add_nearest_weather('portland')
+add_nearest_weather('columbus')
+# add_nearest_weather('portland')
 # add_nearest_weather('boston')
 
-
+# portland_bss = pd.read_csv('../final-bss-data/portland_bss.csv')
+# boston_bss = pd.read_csv('../final-bss-data/boston_bss.csv')
 # columbus_bss = pd.read_csv('../final-bss-data/columbus_bss.csv')
 # columbus_bss.start_datetime.iloc[5175]
 # weather = pd.read_csv('columbus-updated-weather.csv')
+# good_weather = weather[weather.snow.notna()]
+# good_weather.DATE.apply(lambda x: x[:10]).unique()
 
 # last_weather_date = datetime.strptime(weather.DATE.iloc[len(weather) - 1], '%Y-%m-%dT%H:%M:%S')
 # columbus_date = datetime.strptime(columbus_bss.start_datetime.iloc[5175], '%Y-%m-%d %H:%M:%S')
+# delta = last_weather_date - columbus_date
+# return (delta.days * 24 + delta.seconds / 3600) # hours
